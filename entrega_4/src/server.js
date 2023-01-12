6309e4b5fff050253268c505356a1b1a59b6110c
José Moreno <josem3748@gmail.com>
2023-01-12 11:24:24 -0300

    Poniendo view adentro de src

--- entrega_4/src/server.js
+++ entrega_4/src/server.js
@@ -18,7 +18,7 @@ const app = express();
 
 const initServer = () => {
   app.set("view engine", "ejs");
-  app.set("views", "../views");
+  app.set("views", "./views");
 
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
