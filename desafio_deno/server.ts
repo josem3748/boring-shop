9b0b7a945e0ee5ea371878df8611adddf7520d23
José Moreno <josem3748@gmail.com>
2023-02-09 12:45:54 -0300

    Desafío: Servidor en Deno

---
+++ desafio_deno/server.ts
@@ -0,0 +1,11 @@
+import { serve } from "https://deno.land/std@0.52.0/http/server.ts";
+
+const port = 8000;
+
+const server = serve({ port: port });
+
+console.log(`http://localhost:${port}/`);
+
+for await (const req of server) {
+  req.respond({ body: "<h1>Hello Deno Server</h1>" });
+}
